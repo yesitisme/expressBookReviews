dbdbe5eb2ef39b9e89628eb39552d56c992b3243
yes it is me <perpetualtalk@gmail.com>
2025-01-19 19:42:40 -0500

    added page two of tasks

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,62 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const session = require('express-session')
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+regd_users.use(session({ secret: "fingerpint" }, resave = true, saveUninitialized = true));
+const secretKey = 'MyBestProjectEver';
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+
+let users = [{ "username": "john", "password": "doe" }];
+
+const isValid = (username) => { //returns boolean
+    //write code to check is the username is valid
+
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+    //write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+    //Write your code here
+    let username = req.body.username;
+    let password = req.body.password;
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
